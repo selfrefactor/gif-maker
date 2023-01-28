913935cbf4f8436dc943f4bd57b876da511d62ee
Deyan Totev <deyan.totev.extern@seven.one>
2023-01-28 18:54:58 +0200

    chore@small

--- gif-maker.js
+++ gif-maker.js
@@ -1,6 +1,15 @@
 const Jimp = require('jimp')
 const JPEG = require('jpeg-js')
-const { mapParallelAsyncWithLimit, splitEvery, mapAsync, dropLast, takeLast } = require('rambdax')
+const {
+  mapParallelAsyncWithLimit,
+  splitEvery,
+  mapAsync,
+  dropLast,
+  takeLast,
+  delay,
+  flatten,
+} = require('rambdax')
+const { existsSync } = require('fs')
 const { scanFolder, execSafe } = require('helpers-fn')
 Jimp.decoders[ 'image/jpeg' ] = data =>
   JPEG.decode(data, { maxMemoryUsageInMB : 2024 })
@@ -10,20 +19,17 @@ if (!subreddit){
   throw new Error('No subreddit input')
 }
 const BATCH_SIZE = 360
-const DEFAULT_DELAYS = [ 22 ].reverse()
+const DEFAULT_DELAY = 22
 
 const GIF_CREATE_ONLY = process.env.GIF_CREATE_ONLY === 'ON'
 const WITH_COVER_FLAG = process.env.WITH_COVER_FLAG === 'ON'
 const WITH_CONTAIN_FLAG = process.env.WITH_CONTAIN_FLAG === 'ON'
-const DELAYS = process.env.DELAYS ?
-  process.env.DELAYS.split(',').map(Number)
-    .reverse() :
-  DEFAULT_DELAYS
+const DELAY = process.env.DELAY ? Number(process.env.DELAY) : DEFAULT_DELAY
 
 console.log({
   WITH_COVER_FLAG,
   WITH_CONTAIN_FLAG,
-  DELAYS,
+  DELAY,
   GIF_CREATE_ONLY,
 })
 
@@ -40,32 +46,52 @@ async function prepareImages(){
   ))
   console.log(images.length, 'images')
   const batches = splitEvery(BATCH_SIZE, images)
-  const modifiedBatches = [...dropLast(2, batches), [...takeLast(2, batches)]]  
+  const modifiedBatches = [
+    ...dropLast(2, batches),
+    flatten(takeLast(2, batches)),
+  ]
+  modifiedBatches.forEach((batch, i) => {
+    console.log(
+      batch.length, i, 'modifiedBatches.length'
+    )
+  })
+  console.log(batches.length, 'modifiedBatches')
   await mapAsync(applyChanges, modifiedBatches)
 }
 
 async function applyChanges(images, i){
-  console.log('batch size', images.length, i)
+  console.log(
+    'batch size', images.length, i
+  )
   const applyResize = async coverFlag => {
     const folder = coverFlag ?
       'resized/downloads-resized-cover' :
       'resized/downloads-resized-contain'
     await mapParallelAsyncWithLimit(
       async (imagePath, i) => {
-        try{
-          console.log(
-            imagePath, coverFlag, i, 'start'
-          )
-          const outputPath = imagePath.replace('/assets/', `/${ folder }-${ i }/`)
+        try {
+          const outputPath = imagePath.replace('/assets/',
+            `/${ folder }-${ i }/`)
+          console.log({
+            imagePath,
+            coverFlag,
+            i,
+            outputPath,
+          }, 'start')
+          if (existsSync(outputPath)){
+            console.log(imagePath, 'exists')
+
+            return
+          }
           const image = await Jimp.read(imagePath)
           console.log(imagePath, 'read')
-  
+
           const method = coverFlag ? 'cover' : 'contain'
           await image[ method ](2000, 1250) // resize
             .quality(100) // set JPEG quality
             .writeAsync(outputPath)
           console.log(imagePath, 'done')
-        }catch(e){
+        } catch (e){
           console.log(e, 'error')
           console.log(imagePath, 'error')
         }
@@ -89,21 +115,15 @@ async function applyChanges(images, i){
       `resized/downloads-resized-cover-${ i }` :
       `resized/downloads-resized-contain-${ i }`
 
-    await mapParallelAsyncWithLimit(
-      async DELAY => {
-        const coverPart = coverFlag ? '-cover' : ''
-        const gifName = `${ subreddit }-${ i }-${ DELAY }${ coverPart }.gif`
-        const gifArguments = `-src="${ folder }/${subreddit}/images/*.jpg" -delay=${ DELAY } -dest="gifs/${ gifName }" -verbose`
-        const gifCommand = `goanigiffy ${ gifArguments }`
-        console.log(gifCommand, 'gifCommand')
-        await execSafe({
-          command : gifCommand,
-          cwd     : __dirname,
-        })
-      },
-      2,
-      DELAYS
-    )
+    const coverPart = coverFlag ? '-cover' : ''
+    const gifName = `${ subreddit }-${ i }-${ DELAY }${ coverPart }.gif`
+    const gifArguments = `-src="${ folder }/${ subreddit }/images/*.jpg" -delay=${ DELAY } -dest="gifs/${ gifName }" -verbose`
+    const gifCommand = `goanigiffy ${ gifArguments }`
+    console.log(gifCommand, 'gifCommand')
+    await execSafe({
+      command : gifCommand,
+      cwd     : __dirname,
+    })
   }
 
   console.log('start gif contain'.toUpperCase())
@@ -115,7 +135,9 @@ async function applyChanges(images, i){
     await applyGif(true)
   }
   console.log('end gif'.toUpperCase())
-  console.log('batch size', images.length, i, 'END')
+  console.log(
+    'batch size', images.length, i, 'END'
+  )
 }
 
 void (async function run(){
